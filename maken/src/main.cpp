1d3d0aaab54a1badc78e1cda2240f48bfe6eb45f
r-neal-kelly <r.neal.kelly@gmail.com>
2021-08-07 00:43:52 -0400

    just minor changes

--- maken/src/main.cpp
+++ maken/src/main.cpp
@@ -13,6 +13,7 @@ int main(int argument_count, char* arguments[])
     for (size_t idx = 0, end = argument_count; idx < end; idx += 1) {
         nkr::some_t<char*> argument = arguments[idx];
         printf("%s\n", argument());
-        printf("%zu", nkr::string_t<>::charcoder_t::value_size());
     }
+
+    printf("%zu", nkr::string_t<>::charcoder_t::value_size());
 }
